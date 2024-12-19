3361e2a275703b1383c422a8c81567425d27a267
Nguyen Nicolas <bumbleplumz@gmail.com>
2024-12-19 23:37:33 +0100

    v1

--- index.js
+++ index.js
@@ -84,6 +84,18 @@
         console.log("Adresse email envoyée :", email); // Remplacez ceci par votre logique `fetch`
         dialog.close(); // Fermer le dialogue après envoi
     });
+
+    const fadeInElements = document.querySelectorAll('.fadein-left');
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    });
+
+    fadeInElements.forEach(el => observer.observe(el));
 });
 
 }())
